41173f38d6e7eebb83722994183c1fff1e463764
brianho123 <hobrian71@gmail.com>
2025-11-08 19:49:25 +0800

    Added components and connected them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Header } from "./components/Header"
+import { Tabs } from "./components/Tabs"
+import { TodoInput } from "./components/TodoInput"
+import { TodoList } from "./components/TodoList"
 
-
+// App component that displays all components
 function App() {
   let x = 3
-
   return (
-    <div>{x}</div>
+    <div>
+      <Header />
+      <Tabs />
+      <TodoList />
+      <TodoInput />
+    </div>
   )
 }
 
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,5 @@
 export function Tabs () {
     return (
-
+        <nav></nav>
     )
 }
