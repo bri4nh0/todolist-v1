7f31cfb315dca57ff801c2998685bd5790b031c2
brianho123 <hobrian71@gmail.com>
2025-11-11 03:24:20 +0800

    Added code for tabs to display task amounts

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,13 +1,20 @@
-export function Tabs () {
+export function Tabs (props) {
+    const { todos } = props
     // Define buttons to be displayed in an array
     const tabs = ['All', 'Open','Completed']
     // Return an array of buttons using a map and unique key identifier
     return (
         <nav className="tab-container">
             {tabs.map((tab, tabIndex) => {
+                const numOfTasks = tab === 'All' ? 
+                    todos.length :
+                    tab === 'Open' ? 
+                        todos.filter(val => !val.complete).
+                        length :
+                        todos.filter(val => val.complete).length
                 return (
                     <button key={tabIndex} className="tab-button">
-                        <h4>{tab} <span>(0)</span></h4>
+                        <h4>{tab} <span>({numOfTasks})</span></h4>
                     </button>
                 )
             })}
