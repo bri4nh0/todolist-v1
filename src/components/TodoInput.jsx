c26f5473774a47403ad86a77ff1fba8cd1c731a7
brianho123 <hobrian71@gmail.com>
2025-11-12 16:46:09 +0800

    Added code for input and card

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,10 @@
 export function TodoInput () {
     return (
-        <div></div>
+        <div className="input-container">
+            <input placeholder="Add task"/>
+            <button>
+                
+            </button>
+        </div>
     )
 }
